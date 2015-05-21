2ee150fd17d5031e32205da015b43daf871e89e0
Rusmlov <alexandria_l@live.com>
2015-05-21 09:52:57 -0300

    Create BetterButton.h

---
+++ BetterButton.h
@@ -0,0 +1,15 @@
+//
+//  BetterButton.h
+//  buttonsAndActionsTest
+//
+//  Created by Phablulo Joel on 27/04/15.
+//  Copyright (c) 2015 Phablulo Joel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface BetterButton : UIButton
+
+
+@end
